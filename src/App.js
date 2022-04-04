e50d0acd60ea3a71b064e9297a57012abacc03d0
Mayra <mayra203@gmail.com>
2022-04-04 12:46:54 -0500

    Agregando el nuevo proyecto de nuevo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import MainHeader from './components/MainHeader';
-import ProductDetail from './pages/ProductDetail';
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Routes>
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/product-detail/:productId" element={<ProductDetail />} />
-        </Routes>
-      </main>
+
     </div>
   );
 }
