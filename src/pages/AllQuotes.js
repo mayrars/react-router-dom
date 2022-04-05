bfb6a71fa56d588fa136b778b43b0604ddcb4597
Mayra <mayra203@gmail.com>
2022-04-05 13:54:55 -0500

    Mostrando los elementos de la lista

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Max', text:'Pellentesque in ipsum id orci porta dapibus.' },
+    {id: 'q2', author: 'Leo', text:'Proin eget tortor risus.' },
+    {id: 'q3', author: 'Sara', text:'Donec rutrum congue leo eget malesuada.' },
+    {id: 'q4', author: 'Martin', text:'Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.' },
+]
 const AllQuotes = () => {
-  return (
-    <div>AllQuotes</div>
-  )
+  return <QuoteList quotes={DUMMY_QUOTES}/>
 }
 
 export default AllQuotes
